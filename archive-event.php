56e8ebed91e4225b719ba190f1cab7583336abdc
Lex <ancash714@gmail.com>
2018-07-03 14:55:01 -0500

    Initial Commit

---
+++ archive-event.php
@@ -0,0 +1,34 @@
+<?php 
+  
+  get_header();
+  pageBanner(array(
+    'title' => 'All Events',
+    'subtitle' => 'See what is going on in our world'
+  ));
+   ?>
+
+  <div class="container container--narrow page-section">
+
+    <?php 
+      while(have_posts()) {
+        the_post(); 
+        get_template_part('template-parts/content-event');
+      } 
+      echo paginate_links();
+    ?>
+
+      <hr class="section-break">
+    <p>Looking for a recap of Past Events? <a href="<?php echo site_url('/past-events') ?>">Chese these out.</a></p>
+
+
+
+
+  </div>
+
+
+
+<?php 
+
+  get_footer();
+
+?>
